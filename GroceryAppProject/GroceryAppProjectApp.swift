c3696226f84a8da565b922ecdef676066768c654
Hamed Haghani <HamedHaghani@yahoo.com>
2025-02-28 11:09:26 -0500

    Initial Commit

---
+++ GroceryAppProject/GroceryAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppProjectApp.swift
+//  GroceryAppProject
+//
+//  Created by HAMED HAGHANI on 2025-02-28.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryAppProjectTests/GroceryAppProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppProjectTests.swift
+//  GroceryAppProjectTests
+//
+//  Created by HAMED HAGHANI on 2025-02-28.
+//
+
+import Testing
+@testable import GroceryAppProject
+
+struct GroceryAppProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
